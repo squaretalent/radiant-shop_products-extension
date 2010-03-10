a0924ef462fb722196f8f8f5ffd06bde5242e5f1
dirkkelly <dirk.kelly@squaretalent.com>
2010-03-10 22:05:07 +0800

    readding prototype

---
+++ prototype/javascripts/admin/shop/products.js
@@ -0,0 +1,15 @@
+Event.addBehavior({
+  'div#shop_product_details': TabControlBehavior(),
+});
+
+function select_shop_product_category() {
+  return false;
+}
+
+function select_shop_product() {
+  return false;
+}
+
+function create_shop_product_category() {
+  return false;
+}
