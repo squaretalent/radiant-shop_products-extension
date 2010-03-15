b9a9e0ac7dbc49d4e4f05fd09a931a6678cee922
dirkkelly <dirk.kelly@squaretalent.com>
2010-03-15 15:11:00 +0800

    very initial javascripts being done, not so great with the prototype, learning as I'm going

--- prototype/javascripts/admin/shop/products.js
+++ prototype/javascripts/admin/shop/products.js
@@ -1,15 +1,60 @@
-Event.addBehavior({
-  'div#shop_product_details': TabControlBehavior(),
+//  Select Shop Category
+//    Category exists, and has a name as well as products
+//    Update primary window to display category and first product
+//    Update products list to show all products
+var SelectShopCategory = Behavior.create({
+  onclick : function(e) {        
+    new Ajax.Request('/admin/products.json', { 
+      method: 'get',
+      onSuccess: function(data) {
+        $('shop_products_list').innerHTML = "";
+        products = data.responseText.evalJSON();
+        
+        products.each(function(product) {
+          li    = new Element('li', { 'class' : "product", 'id' : "shop_product-" + product.sku });
+          span  = new Element('span', { 'class' : "title" })
+          a     = new Element('a', { 'href' : "/product/" + product.sku })
+          
+          $('shop_products_list').appendChild(li.insert(span.insert(a.insert(product.title))));
+        });
+        // Fire click event for first anchor
+        $('shop_product').show();
+      },
+      onFailure: function(data) {
+        $('shop_products_list').innerHTML = "<p>Something went wrong</p>";
+      }
+    });
+    
+    $('shop_product_category_title').value = this.element.text;
+    
+    return false;
+  }
 });
 
-function select_shop_product_category() {
-  return false;
-}
+//  Select Shop Product
+//    Product exists, has a name and infinite properties
+//    Update primary window to display all the product details
+var SelectShopProduct = Behavior.create({
+  onclick : function(e) {
+    $('shop_product_name').value = this.element.text;
+    $('shop_product').show();
+    return false;
+  }
+})
 
-function select_shop_product() {
-  return false;
-}
+var CreateShopCategory = Behavior.create({
+  onclick : function(e) {
+    $('shop_product_category_title').value = "";
+    $('shop_product').hide();
+    return false;
+  }
+});
 
-function create_shop_product_category() {
-  return false;
-}
+Event.addBehavior({
+  'div#shop_product_details': TabControlBehavior(),
+  
+  '#shop_product_categories_list a:not(.new)': SelectShopCategory(),
+  '#shop_products_list a:not(.new)': SelectShopProduct(),
+  
+  '#shop_product_categories_list a.new': CreateShopCategory()
+});
