c5e0247fc1413e916918520e79e7b1efa6431565
dirkkelly <dirk.kelly@squaretalent.com>
2010-03-16 09:07:34 +0800

    More work on javascript, got around the click event by creating exceptions

--- prototype/javascripts/admin/shop/products.js
+++ prototype/javascripts/admin/shop/products.js
@@ -3,30 +3,9 @@
 //    Update primary window to display category and first product
 //    Update products list to show all products
 var SelectShopCategory = Behavior.create({
-  onclick : function(e) {        
-    new Ajax.Request('/admin/products.json', { 
-      method: 'get',
-      onSuccess: function(data) {
-        $('shop_products_list').innerHTML = "";
-        products = data.responseText.evalJSON();
-        
-        products.each(function(product) {
-          li    = new Element('li', { 'class' : "product", 'id' : "shop_product-" + product.sku });
-          span  = new Element('span', { 'class' : "title" })
-          a     = new Element('a', { 'href' : "/product/" + product.sku })
-          
-          $('shop_products_list').appendChild(li.insert(span.insert(a.insert(product.title))));
-        });
-        // Fire click event for first anchor
-        $('shop_product').show();
-      },
-      onFailure: function(data) {
-        $('shop_products_list').innerHTML = "<p>Something went wrong</p>";
-      }
-    });
-    
-    $('shop_product_category_title').value = this.element.text;
-    
+  onclick : function(e) {
+    UpdateShopCategory(this.element);
+    UpdateShopProducts();
     return false;
   }
 });
@@ -36,19 +15,71 @@ var SelectShopCategory = Behavior.create({
 //    Update primary window to display all the product details
 var SelectShopProduct = Behavior.create({
   onclick : function(e) {
-    $('shop_product_name').value = this.element.text;
-    $('shop_product').show();
+    UpdateShopProduct(this.element);
     return false;
   }
 })
 
+//  Create Shop Category
+//    Clear out categories and products
+//    Primary window is now a blank slate for a new category
 var CreateShopCategory = Behavior.create({
   onclick : function(e) {
-    $('shop_product_category_title').value = "";
-    $('shop_product').hide();
+    UpdateShopCategory(this.element);
+    NewShopCategory();
     return false;
   }
 });
+
+function UpdateShopCategory(category) {
+  $('shop_product_categories_list').down('a.current').removeClassName('current');
+  category.addClassName('current');
+  
+  $('shop_product_category_title').value = category.text;
+}
+
+function NewShopCategory() {
+  $('shop_products_list').innerHTML = "";
+  $('shop_product_category_title').value = "";
+  $('shop_product').hide();
+}
+
+function UpdateShopProduct(product) {
+  var product_id = null;
+  
+  if(product == "first") {
+    $('shop_products_list').down('a').addClassName('current');
+    //product_id = 1;
+  } else {
+    $('shop_products_list').down('a.current').removeClassName('current');
+    product.addClassName('current');
+    //product_id = this.element.getAttribute('product_id');
+  }
+  
+  $('shop_product').show();
+}
+
+function UpdateShopProducts() {
+  new Ajax.Request('/admin/products.json', { 
+    method: 'get',
+    onSuccess: function(data) {
+      $('shop_products_list').innerHTML = "";
+      products = data.responseText.evalJSON();
+      
+      products.each(function(product) {
+        li    = new Element('li', { 'class' : "product", 'id' : "shop_product-" + product.sku });
+        span  = new Element('span', { 'class' : "title" })
+        a     = new Element('a', { 'href' : "/product/" + product.sku })
+        
+        $('shop_products_list').appendChild(li.insert(span.insert(a.insert(product.title))));
+      });
+      UpdateShopProduct("first");
+    },
+    onFailure: function(data) {
+      $('shop_products_list').innerHTML = "<p>Something went wrong</p>";
+    }
+  });
+}
 
 Event.addBehavior({
   'div#shop_product_details': TabControlBehavior(),
