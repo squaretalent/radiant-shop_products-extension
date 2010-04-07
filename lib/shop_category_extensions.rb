0b703da7195fa54eedc7334552b8e51ec8a817d3
dirkkelly <dirk.kelly@squaretalent.com>
2010-04-07 09:54:40 +0800

    approaching layouts differently, making them the config value after initialize, makes it more obvious what is being used

--- lib/shop_category_extensions.rb
+++ lib/shop_category_extensions.rb
@@ -2,16 +2,21 @@ module ShopCategoryExtensions
   
   def self.included(base)
     base.class_eval {
+      def after_initialize
+        self.custom_layout = Radiant::Config['shop.category_layout'] if self.custom_layout.nil?
+        self.custom_product_layout = Radiant::Config['shop.product_layout'] if self.custom_product_layout.nil?
+      end
+      
       def slug
         "/shop/category/#{handle}"
       end
       
       def layout
-        result = custom_layout.blank? ? Radiant::Config['shop.category_layout'] : custom_layout
+        custom_layout
       end
       
       def product_layout
-        result = custom_product_layout.blank? ? Radiant::Config['shop.product_layout'] : custom_product_layout
+        custom_product_layout
       end
     }
   end
